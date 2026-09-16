7e6f4c5c0a7fffbc13bd0a243ca081fc6b17764b
Yan <yanphan.dev@gmail.com>
2026-09-16 12:45:09 +0700

    chore: Khoi tao Asset Pipeline & Framework Config (D1-01)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+import Alpine from 'alpinejs';
+import Sortable from 'sortablejs';
+import Chart from 'chart.js/auto';
+
+window.Alpine = Alpine;
+Alpine.start();
+
+window.Sortable = Sortable;
+window.Chart = Chart;
